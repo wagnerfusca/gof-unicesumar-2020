b5aa1771abff81e1c247db2465517275a280b68f
Wagner Mendes Voltz - Fusca <wagner.voltz@matera.com>
2020-12-05 02:08:37 -0300

    alterado o metodo de execucao do visitor

--- src/br/edu/unicesumar/visitor/Main.java
+++ src/br/edu/unicesumar/visitor/Main.java
@@ -3,8 +3,13 @@ package br.edu.unicesumar.visitor;
 public class Main {
 
 	public static void main(String[] args) {
-		Visitor mostra = new MostraDiaAtualVisitor();
-		mostra.visit(new ConcreteElement());
+		ConcreteElement concreteElement = new ConcreteElement();
+		
+		ConcreteVisitor concreteVisitor = new ConcreteVisitor();
+		concreteElement.accept(concreteVisitor);
+		
+		MostraDiaAtualVisitor mostraDiaAtualVisitor = new MostraDiaAtualVisitor();		
+		concreteElement.accept(mostraDiaAtualVisitor);
 		
 		
 	}
